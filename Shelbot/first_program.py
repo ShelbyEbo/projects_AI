cc97ee08bfa960c9891c6b1947f28636c90732be
doriaMAC <doriamac@MacBook-Pro-de-doriaMAC.local>
2024-10-13 10:56:22 +0100

    keeping

--- Shelbot/first_program.py
+++ Shelbot/first_program.py
@@ -6,11 +6,9 @@ chatbot = pipeline("text-generation")
 print("Olá! Como posso lhe ajudar hoje?")
 while 1:
     user_input = input("You: ")
-    if user_input.lower() in ["tchau", "adeus"]:
-        print("Adeus! Continuação de um bom dia :D")
-        break
-    if user_input.upper() in ["Tchau", "Adeus"]:
-        print("Adeus! Continuação de um bom dia :D")
+    if user_input.lower() in ["goodbye", "bye"]:
+        print("Bye! Thank you :D")
         break
     response = chatbot(user_input)
-    print(f"Chatbot: {response}")
+    bot_responde = response[0]['generated_text'].split("Chatbot")
+    print(f"Chatbot: {bot_responde}")
